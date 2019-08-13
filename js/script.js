b0d0bd8c337e1a0b1cb605847e0c542fc6f29a58
Andrew Peterson and dominic montelongo <dominic@>
2019-08-13 14:22:57 -0700

    adds functional paragraph toggle button

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#javascript").click(function() {
+    $(".javaScriptInfo").toggle();
+    console.log("hello");
+  });
+});
+
+
+//   $(".clickable").click(function() {
+//     $("#javascript-info").toggle();
+//     $("javascript").toggle();
+//   });
+// });
